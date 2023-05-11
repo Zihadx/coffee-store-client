6e1e4a71c0f74f08f86af8af2bcf2a52db9d398f
zihadx <blankzihad@gmail.com>
2023-05-11 12:14:00 +0600

    setup project

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h1>Add coffee</h1>
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const updateCoffee = () => {
+    return (
+        <div>
+            <h1>Update coffee</h1>
+        </div>
+    );
+};
+
+export default updateCoffee;
